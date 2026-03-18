66504f288745cf6a03fa434958425ba3b141c7f2
colin.kueer@student.htl-rankweil.at <colin.kueer@student.htl-rankweil.at>
2026-03-18 10:23:10 +0000

    Created Startseite.leihen.RowTemplate3; Edited Startseite.leihen

--- client_code/Startseite/leihen/__init__.py
+++ client_code/Startseite/leihen/__init__.py
@@ -12,3 +12,36 @@ class leihen(leihenTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+    sql = """SELECT 
+    AusleihID as id,
+    Ausleihdatum as ausleihdatum,
+    geplantesrueckgabedatum as geplantesrd,
+    tatsaechlichesrueckgabedatum as tatsaechlichesrd,
+    FKISBN as isbn,
+    FKKundennummer as kundenid
+    FROM Leihe
+    """
+
+    data = anvil.server.call('query_database_dict', f'{sql}')
+
+    self.repeating_panel_1.items = data
+
+  @handle("button_menue", "click")
+  def button_menue_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    open_form('Startseite')
+
+  @handle("button_kunden", "click")
+  def button_kunden_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  @handle("button_buecher", "click")
+  def button_buecher_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  @handle("button_verlag", "click")
+  def button_verlag_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
